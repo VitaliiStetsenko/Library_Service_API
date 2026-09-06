267b08d3b3725c9cabdb185129ac3a4ed84b78e4
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2026-09-06 18:42:06 +0200

    added pagination

--- books/views.py
+++ books/views.py
@@ -1,8 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from books.models import Book
 from books.permisions import AdminAllOrReadOnly
 from books.serializers import BookSerializer
+
+
+class DefaultPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = "page_size"
+    max_page_size = 20
 
 
 class BooksViewSet(viewsets.ModelViewSet):
