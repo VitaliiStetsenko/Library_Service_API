b73402a4da191fea379627b8d408524673f3b71b
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2026-09-06 18:46:50 +0200

    added filtering by fields author & title

--- books/views.py
+++ books/views.py
@@ -13,6 +13,17 @@ class DefaultPagination(PageNumberPagination):
 
 
 class BooksViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [AdminAllOrReadOnly]
+
+    def get_queryset(self):
+        self.queryset = Book.objects.all()
+
+        title = self.request.query_params.get("title", None)
+        author = self.request.query_params.get("author", None)
+
+        if title:
+            self.queryset = self.queryset.filter(title__icontains=title)
+
+        if author:
+            self.queryset = self.queryset.filter(author__icontains=author)
