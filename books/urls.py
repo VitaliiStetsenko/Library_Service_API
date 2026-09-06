858c380b6ea6d25abadb97e5eb97e21f04e1f837
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2026-09-06 16:37:28 +0200

    created serializer, viewset and url path for books

--- books/urls.py
+++ books/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from books.views import BooksViewSet
+
 app_name = "books"
 
-urlpatterns = []
+router = routers.DefaultRouter()
+router.register("books", BooksViewSet)
+
+urlpatterns = [path("", include(router.urls))]
