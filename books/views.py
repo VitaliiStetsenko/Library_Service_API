e4796a6c7a901e284fcfc60402a5360e47cf09b4
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2026-09-06 18:40:23 +0200

    creates permissions

--- books/views.py
+++ books/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 
 from books.models import Book
+from books.permisions import AdminAllOrReadOnly
 from books.serializers import BookSerializer
 
 
 class BooksViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [AdminAllOrReadOnly]
