e4055fbe902ef0601d29bbe33245faa4610a1bcb
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2026-09-06 19:11:12 +0200

    fiexed commas in settings

--- Library_service/settings.py
+++ Library_service/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework.authtoken"
-    "books"
+    "rest_framework.authtoken",
+    "books",
     "users"
 ]
 
