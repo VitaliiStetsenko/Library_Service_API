d220fa3a16d8ca2115a0b959e573da55c852b81d
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2026-09-07 21:55:30 +0200

    created payment viewset

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from books.views import DefaultPagination
+from payment.models import Payment
+from payment.permissions import IsAuthenticatedAndOwnerOrAdmin
+from payment.serializers import PaymentSerializer
+
+
+class PaymentViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticatedAndOwnerOrAdmin]
+    pagination_class = DefaultPagination
+
+    def get_queryset(self):
+        queryset = Payment.objects.select_related(
+            "borrowing",
+            "book",
+            "user",
+        )
+
+        if self.request.user.is_staff:
+            return queryset
+
+        return queryset.filter(user=self.request.user)
