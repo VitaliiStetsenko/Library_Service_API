e81f60f2d9e47c2baeb212b4f8897e21afff313b
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2026-09-07 22:00:06 +0200

    registered in admin user, borrowing, payment models

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from payment.models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "borrowing",
+        "type",
+        "status",
+        "money_to_pay",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "type",
+    )
+
+    search_fields = (
+        "user__email",
+        "session_id",
+    )
