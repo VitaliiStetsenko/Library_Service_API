9f26bfffb12f08658ff3eb0f514af440f116e7d2
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2026-09-07 15:00:16 +0200

    added create serializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,9 +1,29 @@
 from rest_framework import serializers
 
+from books.models import Book
+from books.serializers import BookSerializer
 from borrowings.models import Borrowings
 
 
-class BorrowingsSerializer(serializers.ModelSerializer):
+class BorrowingsListRetrieveSerializer(serializers.ModelSerializer):
+    book = BookSerializer()
+    class Meta:
+        model = Borrowings
+        fields = [
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        ]
+
+class BorrowingsCreateSerializer(serializers.ModelSerializer):
+    actual_return_date = serializers.DateField(read_only=True)
+    book = serializers.PrimaryKeyRelatedField(
+        queryset=Book.objects.filter(inventory__gt=0)
+    )
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = Borrowings
         fields = [
